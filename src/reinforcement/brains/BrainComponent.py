11f0382d697ef8022e826110d9e7b6042f897fd5
Nithid Mahattanasin <nithid.ma@ku.th>
2021-04-23 22:18:22 +0700

    Reverted unwanted files

--- src/reinforcement/brains/BrainComponent.py
+++ src/reinforcement/brains/BrainComponent.py
@@ -1,17 +1,12 @@
 from tensorflow.keras.layers import Input,Dense, Flatten, Conv2D,ReLU,BatchNormalization,Add
-from keras import regularizers
-
-
 def conv_layer(input_block,filter_num,kernel_size,batch_norm = True,add_skip_con = False):
-    # x = Conv2D(filter_num,kernel_size,strides = (1,1),activation = 'linear',padding = 'same',data_format="channels_first")(input_block)
-    x = Conv2D(filter_num,kernel_size,strides = (1,1), activation = 'linear', padding = 'same',data_format="channels_first", use_bias = False, kernel_regularizer=regularizers.l2(0.0001))(input_block)
+    x = Conv2D(filter_num,kernel_size,strides = (1,1),activation = 'linear',padding = 'same',data_format="channels_first")(input_block)
     if batch_norm:
         x = BatchNormalization(axis = 1)(x)
     if add_skip_con:
         x = Add()([input_block,x])
     x = ReLU()(x)
     return x
-
 
 def residual_layer(input_block,filter_num,kernel_size):
 
@@ -26,28 +21,25 @@ def residual_layer(input_block,filter_num,kernel_size):
 
 def input_conv_layer():
     main_input = Input(shape = (3,6,7), name = 'main_input')
-    x = conv_layer(input_block = main_input,filter_num = 64 , kernel_size= (4,4))
+    x = conv_layer(input_block = main_input,filter_num = 128 , kernel_size= (4,4))
     return main_input , x
 
-def residual_tower (input_block,res_number = 5): 
-    x = residual_layer(input_block=input_block,filter_num=64,kernel_size=(4,4))
+def residual_tower (input_block,res_number = 19): 
+    x = residual_layer(input_block=input_block,filter_num=128,kernel_size=(4,4))
     for num in range(res_number-1):
-        x = residual_layer(input_block=x,filter_num=64,kernel_size=(4,4))
+        x = residual_layer(input_block=x,filter_num=128,kernel_size=(4,4))
     return x
 
 def policy_head (input_block):
     x = conv_layer(input_block=input_block,filter_num=2,kernel_size=(1,1))
     x = Flatten()(x)
-    # x = Dense(7,activation='softmax',name = 'policy_head')(x)
-    x = Dense(7,activation='softmax', use_bias = False, kernel_regularizer=regularizers.l2(0.0001), name = 'policy_head')(x)
+    x = Dense(7,activation='softmax',name = 'policy_head')(x)
     return x
 
 def value_head (input_block):
     x = conv_layer(input_block=input_block,filter_num=1,kernel_size=(1,1))
     x = Flatten()(x)
-    # x = Dense(64,activation='linear')(x)
-    x = Dense(64,activation='linear',use_bias = False, kernel_regularizer=regularizers.l2(0.0001))(x)
+    x = Dense(64,activation='linear')(x)
     x = ReLU()(x)
-    # x = Dense(1,activation='tanh',name = 'value_head')(x)
-    x = Dense(1,activation='tanh', use_bias = False, kernel_regularizer=regularizers.l2(0.0001), name = 'value_head')(x)
+    x = Dense(1,activation='tanh',name = 'value_head')(x)
     return x
--- src/reinforcement/playerVsAI.py
+++ src/reinforcement/playerVsAI.py
@@ -5,27 +5,18 @@ from GameBoard import Connect4Board
 from players.ZeroPlayer import ZeroPlayer
 from brains.ZeroBrain import ZeroBrain
 
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
-
-config = ConfigProto()
-config.gpu_options.allow_growth = True
-session = InteractiveSession(config=config)
-
-ZeroAI =ZeroBrain('Alpha30Hr.h5')
-Zero_player = ZeroPlayer(ZeroAI)
-
+ZeroAI =ZeroBrain(5)
 board = Connect4Board(first_player=1) # first_player = 1 or first_player = 2 
 
 while(board.isEnd is not True):
-    print("ffRound No : {}".format(board.round))
+    print("Round No : {}".format(board.round))
     print("This is what board does look like for player {}".format(board.current_turn))
     board.showBoard()
-    if board.current_turn == 2 :
+    if board.current_turn == 1 :
         x = int(input("enter column (0 - 6) of playerNo {0} : ".format(board.current_turn)))
     else:
-        action, policy = Zero_player.act(board)
-        x = action
+        policy,v = ZeroAI.predict(board.getStateAsPlayer())
+        x = np.argmax(policy)
         print("policy is : {}".format(policy))
     board.insertColumn(x)
 print("Winner is {}".format(board.winner))
